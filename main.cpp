d8a3038f9b805b231fc3ca16fcf55d472af37901
yuyabu <yuyabushita@gmail.com>
2019-10-28 15:42:56 +0900

    auto and const

--- main.cpp
+++ main.cpp
@@ -3,11 +3,16 @@
 void hellowolrd();
 void some_function();
 void init_syntax();
+void auto_init();
+void const_test();
+constexpr int square(int v);
 
 int main(){
     hellowolrd();
     some_function();
     init_syntax();
+    auto_init();
+    const_test();
 }
 
 void hellowolrd(){
@@ -26,4 +31,18 @@ void init_syntax(){
     //int i2 {7.2};//エラー：浮動小数点から整数への変換
     //int i3 = {7.2};//エラー：浮動小数点から整数への変換（=は省略可）
     std::cout << i1 << std::endl;//7
+}
+void auto_init(){
+    int i1 = 7;
+    auto i2 = 7;
+    std::cout << i2 << std::endl;
+}
+void const_test(){
+    const int i=7;
+    //i++; エラー
+    const int i2 = square(6);
+    std::cout << i2 << std::endl;
+}
+constexpr int square(int v){
+    return v*v;
 }
