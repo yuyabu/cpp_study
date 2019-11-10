12357d098ae8e7f6898d47039a67a13b63b448e9
yuyabu <yuyabushita@gmail.com>
2019-11-10 21:10:55 +0900

    add exception code

--- Vector.cpp
+++ Vector.cpp
@@ -8,6 +8,9 @@ Vector::Vector(int s)
 
 double& Vector::operator[](int i)
 {
+    //if(i<0 || size() <= i)throw out_of_range{"Vector::operator[]"};
+    //例外型out_of_rangeの定義が必要っぽい
+
     return elem[i];
 }
 int Vector::size(){
